f945a255659970ce0b340bc473a42a056074fc40
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2024-10-11 11:19:36 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14476 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,26 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         int r = 31;
-        int M = 1234567891;
+        long M = 1234567891l;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int L = Integer.parseInt(br.readLine());
         String s = br.readLine();
 
-        int n = 0;
-        int result = 0;
+        // long 사용
+//        long n = 0l;
+//        long result = 0l;
+//        for (int i=0; i<L; i++) {
+//            n = s.charAt(i) - 96;
+//            result += (long)(n*Math.pow(r, i));
+//        }
+
+        // BigInteger 사용
+        BigInteger result = new BigInteger("0");
         for (int i=0; i<L; i++) {
-            n = s.charAt(i) - 96;
-            result += (n*Math.pow(r, i));
+            result = result.add(BigInteger.valueOf(s.charAt(i)-96).multiply(BigInteger.valueOf(31).pow(i)));
         }
 
-        if (result > M) {
-            System.out.println(result/M);
-        } else {
-            System.out.println(result);
-        }
+        System.out.print(result.remainder(BigInteger.valueOf(M)));
     }
 }
