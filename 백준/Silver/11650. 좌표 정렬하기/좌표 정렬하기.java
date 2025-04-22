2ea77ec60e67d720512331f7e3cab807fa34eb6e
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2025-04-22 14:03:19 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 780 ms, Memory: 67676 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,48 +1,42 @@
+/*
+* 객체 -> 배열
+* 메모리 체크
+* StringBuilder 사용
+* */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static class Plane {
-        int x;
-        int y;
-
-        public Plane(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bufferedReader.readLine());
 
-        Plane[] planes = new Plane[N];
+        int[][] planes = new int[N][2];
         for (int i = 0; i < N; i++) {
             String[] s = bufferedReader.readLine().split(" ");
-            planes[i] = new Plane(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
+            planes[i][0] = Integer.parseInt(s[0]);
+            planes[i][1] = Integer.parseInt(s[1]);
         }
 
-        Arrays.sort(planes, new Comparator<Plane>() {
+        Arrays.sort(planes, new Comparator<int[]>() {
             @Override
-            public int compare(Plane o1, Plane o2) {
-                if (o1.x > o2.x) {
-                    return 1;
-                } else if (o1.x < o2.x) {
-                    return -1;
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
                 } else {
-                    if (o1.y > o2.y) {
-                        return 1;
-                    } else {
-                        return -1;
-                    }
+                    return o1[0] - o2[0];
                 }
             }
         });
 
-        for (Plane plane : planes) {
-            System.out.println(plane.x + " " + plane.y);
+        StringBuilder sb = new StringBuilder();
+        for (int[] plane : planes) {
+            sb.append(plane[0] + " " + plane[1] + "\n");
         }
+
+        System.out.println(sb);
+
     }
 }
