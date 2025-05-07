a99194f8787dd09afcb537da72127be6661aee34
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2025-05-07 16:52:56 +0900

    [Silver IV] Title: 제로, Time: 204 ms, Memory: 23948 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0) {
+                stack.pop();
+            } else {
+                stack.push(input);
+            }
+        }
+
+        int sum = 0;
+        for (Integer i : stack) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
