740cfb518908090949148d156b656b89f7981861
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2026-01-21 08:18:06 +0900

    [level 0] Title: 특수문자 출력하기, Time: 123.08 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         
-        System.out.println("!@#$%^&*(\\\'\"<>?:;");
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
         
     }
 }
