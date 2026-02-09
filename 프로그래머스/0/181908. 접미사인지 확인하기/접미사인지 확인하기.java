47d841059c626a9f4d1fb9adba6d6cc2ced2c7b3
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2026-02-10 08:47:18 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,6 +1,19 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
         int answer = 0;
+
+        String[] suffix = new String[my_string.length()];
+
+        for (int i = 0; i < my_string.length() ; i++) {
+            suffix[i] = my_string.substring(i, my_string.length());
+        }
+
+        for (String suf : suffix) {
+            if (suf.equals(is_suffix)) {
+                answer = 1;
+            }
+        }
+
         return answer;
     }
 }
