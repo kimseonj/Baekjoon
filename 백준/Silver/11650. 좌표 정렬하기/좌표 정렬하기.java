d1838e0a99c693fdbce6474cda1a965b341c854e
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2026-04-14 17:46:57 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 728 ms, Memory: 54352 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,42 +1,30 @@
-/*
-* 객체 -> 배열
-* 메모리 체크
-* StringBuilder 사용
-* */
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[][] arrays = new int[N][2];
 
-        int[][] planes = new int[N][2];
         for (int i = 0; i < N; i++) {
-            String[] s = bufferedReader.readLine().split(" ");
-            planes[i][0] = Integer.parseInt(s[0]);
-            planes[i][1] = Integer.parseInt(s[1]);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arrays[i][0] = Integer.parseInt(st.nextToken());
+            arrays[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(planes, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] == o2[0]) {
-                    return o1[1] - o2[1];
-                } else {
-                    return o1[0] - o2[0];
-                }
+        Arrays.sort(arrays, (a, b) -> {
+            if (a[0] == b[0]) {
+                return Integer.compare(a[1], b[1]);
             }
+            return Integer.compare(a[0], b[0]);
         });
 
         StringBuilder sb = new StringBuilder();
-        for (int[] plane : planes) {
-            sb.append(plane[0] + " " + plane[1] + "\n");
+        for (int i = 0; i < N; i++) {
+            sb.append(arrays[i][0] + " " + arrays[i][1] + "\n");
         }
-
         System.out.println(sb);
-
     }
 }
