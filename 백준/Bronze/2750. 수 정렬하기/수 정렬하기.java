acfe3f43c4f0e9ff0b43dc9e4e8eb3d28ce45efb
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2026-04-14 16:26:43 +0900

    [Bronze II] Title: 수 정렬하기, Time: 112 ms, Memory: 14488 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -6,11 +6,14 @@ import java.util.StringTokenizer;
  * 수 정렬하기
  *
  * N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
- *
+ * 제한시간 2초 -> 약 4000만번 연산
+ * 
  * N(1 ≤ N ≤ 1,000)
  * 절댓값이 1,000보다 작거나 같은 정수
+ * 1000*1000 = 100만번
  *
  * 배열사용?
+ * 
  */
 public class Main {
     public static void main(String[] args) throws IOException {
