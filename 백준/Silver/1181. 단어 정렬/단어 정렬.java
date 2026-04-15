6b89a90887fb66851fe13e7d71f4287e33f3ed98
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2026-04-15 16:35:28 +0900

    [Silver V] Title: 단어 정렬, Time: 336 ms, Memory: 24812 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,46 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
+/** 
+ * 알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오.
+ * 
+ * 길이가 짧은 것부터
+ * 길이가 같으면 사전 순으로
+ * 단, 중복된 단어는 하나만 남기고 제거
+ */
+
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
-        // index, length
-        Map<Integer, String> map = new HashMap<>();
         Set<String> set = new HashSet<>();
-
         for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-            map.put(s.length(), null);
-            set.add(s);
+            set.add(br.readLine());
         }
 
-        Set<Integer> keySet = map.keySet();
-        Object[] array = keySet.toArray();
-//        System.out.println(Arrays.toString(array));
-        Arrays.sort(array);
-//        System.out.println(Arrays.toString(array));
-
-        Object[] arr = set.toArray();
-        Arrays.sort(arr);
-//        System.out.println(Arrays.toString(arr));
-
-        for (Object j : array) {
-            for (int i = 0; i < arr.length; i++) {
-                if (((String)arr[i]).length() == (Integer)j) {
-                    sb.append((String) arr[i] + "\n");
-                }
+        String[] arr = set.toArray(new String[0]);
+        Arrays.sort(arr, (a, b) -> {
+            if (a.length() == b.length()) {
+                return a.compareTo(b);
             }
+            return Integer.compare(a.length(), b.length());
+        });
+     
+        StringBuilder sb = new StringBuilder();
+        for (String word : arr) {
+            sb.append(word).append('\n');
         }
 
-        System.out.println(sb);
-
+        System.out.print(sb);
     }
 }
