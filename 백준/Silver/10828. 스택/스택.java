39558f21d27a2d3d1ff1b9180903f5e1e3306a70
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2026-04-17 02:47:41 +0900

    [Silver IV] Title: 스택, Time: 140 ms, Memory: 18236 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,44 +1,77 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
+
+/** 
+ * 자연수를 원소로 갖는 공집합이 아닌 두 집합 A와 B
+ * 두 집합의 대칭 차집합의 원소의 개수를 출력
+ * 
+ * (A-B)와 (B-A)의 합집합을 A와 B의 대칭 차집합
+ * 
+ * 2초
+ * 
+ * 0 < A원소, B원소 <= 200,000
+ * 
+ * 
+ * 14
+push 1
+push 2
+top
+size
+empty
+pop
+pop
+pop
+size
+empty
+pop
+push 3
+empty
+top
+ */
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringBuilder sb = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
-
         int N = Integer.parseInt(br.readLine());
+
+        Deque<String> stack = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
         for (int i = 0; i < N; i++) {
-            String input = br.readLine();
-            if (input.equals("top")) {
-                if (stack.isEmpty()) {
-                    sb.append("-1" + "\n");
-                } else {
-                    sb.append(stack.peek() + "\n");
-                }
-            } else if (input.equals("size")) {
-                sb.append(stack.size() + "\n");
-            } else if (input.equals("empty")) {
-                if (stack.isEmpty()) {
-                    sb.append("1" + "\n");
-                } else {
-                    sb.append("0" + "\n");
-                }
-            } else if (input.equals("pop")) {
-                if (stack.isEmpty()) {
-                    sb.append("-1" + "\n");
-                } else {
-                    sb.append(stack.pop() + "\n");
-                }
-            } else {
-                int a = Integer.parseInt(input.split(" ")[1]);
-                stack.push(a);
-            }
+            st = new StringTokenizer(br.readLine());
+
+            switch(st.nextToken()) {
+                case "push":
+                    stack.push(st.nextToken());
+                    break;
+                case "pop":
+                    if (stack.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(stack.pop()).append("\n");
+                    }
+                    break;
+                case "empty":
+                    if (stack.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    };
+                    break;
+                case "size":
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "top":
+                    if (stack.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(stack.peek()).append("\n");
+                    }
+            }    
         }
 
         System.out.println(sb);
     }
+
 }
