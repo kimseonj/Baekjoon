af9d9fbe3e87fd013fc82c087082099c5deaaea9
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2026-04-17 03:19:39 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14932 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,45 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Objects;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
+
+/** 
+ * 
+ */
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        for (int n = 0; n < N; n++) {
-            String readLine = br.readLine();
+        Deque<String> stack;
+        for (int i = 0; i < N; i++) {
+            stack = new ArrayDeque<>();
 
-            Stack<Character> stack = new Stack<>();
-            boolean flag = false;
-            for (int i = 0; i < readLine.length(); i++) {
-                char c = readLine.charAt(i);
+            String[] input = br.readLine().split("");
 
-                if (c == '(') {
-                    stack.push(c);
-                } else if (c == ')' && !stack.empty()) {
-                    if (stack.peek() == '(') {
+            for (String s : input) {
+                if (s.equals("(")) {
+                    stack.push("(");
+                } else {
+                    if (!stack.isEmpty() && stack.peek().equals("(")) {
                         stack.pop();
                     } else {
-                        flag = true;
-                        break;
+                        stack.push(")");
                     }
-                } else {
-                    flag = true;
                 }
             }
 
-            // 결과
-            if (flag || !stack.empty()) {
-                System.out.println("NO");
-            } else {
-                System.out.println("YES");
-            }
+            System.out.println(stack.isEmpty() ? "YES" : "NO");
         }
+
+        
     }
+
 }
