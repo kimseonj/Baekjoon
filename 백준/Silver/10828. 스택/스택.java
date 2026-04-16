1bc0372a7bf071f0269eefcf63719f39fc5e654d
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2026-04-17 02:50:57 +0900

    [Silver IV] Title: 스택, Time: 148 ms, Memory: 18300 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -2,31 +2,6 @@ import java.io.*;
 import java.util.*;
 
 /** 
- * 자연수를 원소로 갖는 공집합이 아닌 두 집합 A와 B
- * 두 집합의 대칭 차집합의 원소의 개수를 출력
- * 
- * (A-B)와 (B-A)의 합집합을 A와 B의 대칭 차집합
- * 
- * 2초
- * 
- * 0 < A원소, B원소 <= 200,000
- * 
- * 
- * 14
-push 1
-push 2
-top
-size
-empty
-pop
-pop
-pop
-size
-empty
-pop
-push 3
-empty
-top
  */
 
 public class Main {
