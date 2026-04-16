85f1207b667599ffed09151c11b48b811f802ac1
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2026-04-16 15:14:46 +0900

    [Silver IV] Title: 듣보잡, Time: 248 ms, Memory: 26180 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,10 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
+/** 
+ * M개의 줄에 각각의 문제에 대한 답
+ * 입력으로 숫자가 들어왔다면 그 숫자에 해당하는 포켓몬의 이름을,
+ * 문자가 들어왔으면 그 포켓몬의 이름에 해당하는 번호를 출력
+ * 집합 S에 포함되어 있는 것이 총 몇 개
+ * 
+ * 2초
+ * N과 M (1 ≤ N, M ≤ 500,000)
+ * 
+ * 이중For문 돌면 시간초과 발생함 250,000,000,000번
+ * 
+ */
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,29 +22,28 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Set<String> noHear = new HashSet<String>();
-
+        HashSet<String> set = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            noHear.add(br.readLine());
+            set.add(br.readLine());
         }
 
-        StringBuilder sb = new StringBuilder();
-        ArrayList<String> list = new ArrayList<>();
+        int k = 0;
+        List<String> list = new ArrayList<>();
         for (int i = 0; i < M; i++) {
-            String str = br.readLine();
-
-            if (noHear.contains(str)) {
-                list.add(str);
+            String question = br.readLine();
+            if (set.contains(question)) {
+                list.add(question);
             }
         }
 
-        list.sort(String::compareTo);
-
-        System.out.println(list.size());
-        for (int i = 0; i < list.size(); i++) {
-            sb.append(list.get(i)).append('\n');
+        StringBuilder sb = new StringBuilder();
+        Collections.sort(list);
+        for (String a : list) {
+            sb.append(a).append("\n");
         }
 
+        System.out.println(list.size());
         System.out.println(sb);
     }
+
 }
