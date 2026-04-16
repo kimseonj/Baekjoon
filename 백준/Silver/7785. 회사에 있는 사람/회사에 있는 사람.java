970afc81f27153f9d5f742c7ec8a938b1972b4bf
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2026-04-16 15:27:25 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 660 ms, Memory: 51264 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,36 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.Array;
+import java.io.*;
 import java.util.*;
+
+/** 
+ * 첫째 줄에 로그에 기록된 출입 기록의 수 n
+ * "enter"인 경우는 출근, "leave"인 경우는 퇴근
+ * 동명이인이 없으며, 대소문자가 다른 경우에는 다른 이름
+ * 사람들의 이름은 알파벳 대소문자로 구성된 5글자 이하의 문자열
+ * 
+ * 1초
+ * 
+ */
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        HashMap<String, Boolean> map = new HashMap<>();
-
+        StringTokenizer st;
+        HashSet<String> set = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            String[] arr = br.readLine().split(" ");
-//            System.out.println(arr[0]);
-//            System.out.println(arr[1].equals("enter") ? true : false);
-            map.put(arr[0], arr[1].equals("enter") ? true : false);
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String check = st.nextToken();
+
+            if (check.equals("enter")) {
+                set.add(name);
+            } else {
+                set.remove(name);
+            }
+            
         }
 
-        List<String> keySet = new ArrayList<>(map.keySet());
-        Collections.sort(keySet,Collections.reverseOrder());
-
+        List<String> list = new ArrayList<>(set);
+        Collections.sort(list, Collections.reverseOrder());
+            
         StringBuilder sb = new StringBuilder();
-        for (String key : keySet) {
-            if(map.get(key) == true) {
-                sb.append(key+"\n");
-            }
+        for (String a : list) {
+            sb.append(a).append("\n");
         }
 
         System.out.println(sb);
-
     }
+
 }
