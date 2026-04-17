bc7269de99ae87fd50e8729cba622274a66f51b8
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2026-04-17 16:05:37 +0900

    [Silver II] Title: 스택 수열, Time: 280 ms, Memory: 26332 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,45 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-import java.util.stream.Stream;
+
+/** 
+ * 
+ */
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
-        int[] array = new int[N];
-        for (int i = 0; i < N; i++) {
-            array[i] = Integer.parseInt(br.readLine());
-        }
-
         Deque<Integer> stack = new ArrayDeque<>();
-        int index = 0;
-        int j = 1;
-        while (index < N && j < N + 2) {
-            if (stack.isEmpty()) {
-                stack.push(j);
-                sb.append("+").append("\n");
-            } else if (stack.peek() == array[index]) {
-                stack.pop();
-                index++;
-                sb.append("-").append("\n");
-                continue;
-            } else if (j < N+1){
-                stack.push(j);
-                sb.append("+").append("\n");
+        StringBuilder sb = new StringBuilder();
+        
+        boolean valid = true;
+        int i = 1;
+        for (int k = 0; k < N; k++) {
+            int input = Integer.parseInt(br.readLine());
+
+            while (i <= input) {
+                stack.push(i++);
+                sb.append("+\n");
             }
 
-            j++;
+            if (stack.isEmpty() || stack.peek() != input) {
+                valid = false;
+                break;
+            }
+
+            stack.pop();
+            sb.append("-\n");
         }
 
-        if (stack.isEmpty()) {
+        if (valid && stack.isEmpty()) {
             System.out.println(sb);
         } else {
             System.out.println("NO");
         }
     }
+
 }
