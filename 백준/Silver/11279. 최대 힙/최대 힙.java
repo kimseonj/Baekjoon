54ada20d01e78fea38611cfdd932c0090676c4d2
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2026-04-21 23:06:38 +0900

    [Silver II] Title: 최대 힙, Time: 296 ms, Memory: 26276 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+/** 
+ * 2초
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0) {
+                if (pq.isEmpty()) {
+                    sb.append("0\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.offer(input);
+            }
+            
+        }
+
+        System.out.println(sb);
+        
+        
+    }
+}
